ac312b4033202d64f9d433468eb360b960985273
mark-williams <markwill@hotmail.co.uk>
2017-12-30 10:37:21 +0000

    Show toast message when game lost (mine exploded)

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,24 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import { ToastContainer, toast } from 'react-toastify';
 import { newGame } from '../actions';
 import Board from './Board';
 import logo from '../logo.svg';
 import '../style/App.css';
 
+const gameOverToastOptions = {
+  type: toast.TYPE.WARNING
+};
+
 class App extends Component {
   render() {
+    if (this.props.explodedMineKey) {
+      toast('Game over! You lost.', gameOverToastOptions);
+    }
     return (
       <div className="App">
+        <ToastContainer />
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
         </header>
@@ -23,11 +32,16 @@ class App extends Component {
 }
 
 App.propTypes = {
+  cells: PropTypes.arrayOf(PropTypes.arrayOf(PropTypes.object)),
+  explodedMineKey: PropTypes.string,
   onNewGame: PropTypes.func
 };
 
-const mapStateToProps = () => {
-  return {};
+const mapStateToProps = (state) => {
+  return {
+    cells: state.cells,
+    explodedMineKey: state.explodedMineKey
+  };
 };
 
 const mapDispatchToProps = (dispatch) => {
