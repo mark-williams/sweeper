ae6216c6b213ee24b1eee7cf676b0107dfacf851
mark-williams <markwill@hotmail.co.uk>
2018-01-18 10:22:43 +0000

    Removed erroneous slash in import path

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import minesReducer from './reducers/minesReducer';
 import './style/index.css';
-import App from './components//App';
+import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
 const store = createStore(minesReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
